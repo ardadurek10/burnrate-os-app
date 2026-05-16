862387097800b8ceea890951d8c28fb70c3600e0
ardadurek10 <ardadurek10@gmail.com>
2026-05-17 01:02:13 +0300

    Delete app/page.js

--- app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <html>
-      <body>
-        <script dangerouslySetInnerHTML={{__html: "window.location.replace('https://burnrate-os-app.vercel.app/landing')"}} />
-      </body>
-    </html>
-  )
-}
